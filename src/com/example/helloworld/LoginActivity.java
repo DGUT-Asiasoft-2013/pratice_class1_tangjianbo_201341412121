c9aeaf7ccf1c206dd62e10c18c7e0e7d5f23f93e
Administrator <Administrator@7A304-0006>
2016-12-06 10:02:12 +0800

    新增登录界面

--- src/com/example/helloworld/LoginActivity.java
+++ src/com/example/helloworld/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.example.helloworld;
 
+import com.example.helloworld.fragments.inputcells.SimpleTextInputCellFragment;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class LoginActivity extends Activity {
+	SimpleTextInputCellFragment fragAccout,fragPassword;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,6 +22,36 @@ public class LoginActivity extends Activity {
 				goRegister();
 			}
 		});
+
+		findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				goLogin();	
+			}
+		});
+
+		fragAccout=(SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_account);
+		fragPassword=(SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_password);
+
+	}
+
+	@Override
+	protected void onResume() {
+
+		super.onResume();
+
+		fragAccout.setLabelText("账户名");
+		fragAccout.setHintText("请输入账户名");
+		fragPassword.setLabelText("密码");
+		fragPassword.setHintText("请输入密码");
+		fragPassword.setIsPassword(true);
+	}
+
+	void goLogin() {
+
+
 	}
 
 	void goRegister(){
--- src/com/example/helloworld/RegisterActivity.java
+++ src/com/example/helloworld/RegisterActivity.java
@@ -8,8 +8,10 @@ import android.os.Bundle;
 
 public class RegisterActivity extends Activity {
 	SimpleTextInputCellFragment fragInputCellAccount;
+	SimpleTextInputCellFragment fragInputCellEmailAddress;
 	SimpleTextInputCellFragment fragInputCellPassword;
 	SimpleTextInputCellFragment fragInputCellPasswordRepeat;
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class RegisterActivity extends Activity {
 		setContentView(R.layout.activity_register);
 		
 		fragInputCellAccount = (SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_account);
+		fragInputCellEmailAddress=(SimpleTextInputCellFragment)getFragmentManager().findFragmentById(R.id.input_email);
 		fragInputCellPassword = (SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_password);
 		fragInputCellPasswordRepeat = (SimpleTextInputCellFragment) getFragmentManager().findFragmentById(R.id.input_password_repeat);
 		
@@ -31,12 +34,17 @@ public class RegisterActivity extends Activity {
 		
 		fragInputCellAccount.setLabelText("账户名");
 		fragInputCellAccount.setHintText("请输入账户名");
+		
 		fragInputCellPassword.setLabelText("密码");
 		fragInputCellPassword.setHintText("请输入密码");
+		fragInputCellPassword.setIsPassword(true);
+		
 		fragInputCellPasswordRepeat.setLabelText("重复密码");
 		fragInputCellPasswordRepeat.setHintText("请重复输入密码");
 		fragInputCellPasswordRepeat.setIsPassword(true);
 		
+		fragInputCellEmailAddress.setLabelText("电子邮件");
+		fragInputCellEmailAddress.setHintText("请输入电子邮箱");
 	}
 
 }
