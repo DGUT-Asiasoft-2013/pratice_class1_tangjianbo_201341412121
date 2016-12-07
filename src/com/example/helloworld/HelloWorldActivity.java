066aee660fa30894353f5f5071d57721e52caef8
Administrator <Administrator@7A304-0006>
2016-12-07 10:36:27 +0800

    对于选项按钮添加对应页面

--- src/com/example/helloworld/HelloWorldActivity.java
+++ src/com/example/helloworld/HelloWorldActivity.java
@@ -1,15 +1,66 @@
 package com.example.helloworld;
 
+import com.example.helloworld.fragments.pages.FeedListFragment;
+import com.example.helloworld.fragments.pages.MyProfileFragment;
+import com.example.helloworld.fragments.pages.NoteListFragment;
+import com.example.helloworld.fragments.pages.SearchPageFragment;
+import com.example.helloworld.fragments.widget.MainTabbarFragment;
+import com.example.helloworld.fragments.widget.MainTabbarFragment.OnTabSelectedListener;
+
 import android.app.Activity;
+import android.app.Fragment;
 import android.os.Bundle;
 
 public class HelloWorldActivity extends Activity {
+	FeedListFragment contentFeedList =new FeedListFragment();
+	NoteListFragment contentNoteList =new NoteListFragment();
+	SearchPageFragment contentSearchPage=new SearchPageFragment();
+	MyProfileFragment contentMyprofile =new MyProfileFragment();
+	
+	MainTabbarFragment tabbar;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_helloworld);
+		
+		tabbar = (MainTabbarFragment)getFragmentManager().findFragmentById(R.id.frag_tabber);
+		tabbar.setOnTabSelectedListener(new OnTabSelectedListener() {
+			
+			@Override
+			public void onTabSelected(int index) {
+				changeContentFragment(index);
+				
+			}
+		});
+	}
+	
+	@Override
+	protected void onResume() {
+		
+		super.onResume();
+		
+		tabbar.setSelectedItem(0);
+	}
+	
+	void changeContentFragment(int index){
+		Fragment newFrag=null;
+		switch(index){
+		case 0: newFrag=contentFeedList;break;
+		case 1: newFrag=contentNoteList;break;
+		case 2: newFrag=contentSearchPage;break;
+		case 3: newFrag=contentMyprofile;break;
+		
+		default:break;
+		}
+		
+		if(newFrag==null)return;
+		
+		getFragmentManager()
+		.beginTransaction()
+		.replace(R.id.content, newFrag)
+		.commit();
 	}
 
 }
--- src/com/example/helloworld/PasswordRecoverActivity.java
+++ src/com/example/helloworld/PasswordRecoverActivity.java
@@ -31,6 +31,11 @@ public class PasswordRecoverActivity extends Activity {
 	void goStep2(){
 		getFragmentManager()
 		.beginTransaction()
+		.setCustomAnimations(
+				R.animator.slide_in_right, 
+				R.animator.slide_out_left, 
+				R.animator.slide_in_left, 
+				R.animator.slide_out_right)
 		.replace(R.id.container, step2)
 		.addToBackStack(null)
 		.commit();
